0a1d794fa9262ee5e331dc2f975f7af46eb8c390
Giulia Celora <giulia.celora@dtc.ux.ac.uk>
2019-01-14 19:08:38 +0000

    new algorithm

---
+++ functions.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python
+import pdb 
+import scipy.io as sio
+import numpy as np
+class par_type(object):
+    def __init__(self,sigma,**kwargs):
+        for item in kwargs:
+		self.__dict__.update(kwargs)
+	self.lmbda=0.18;
+	self.gamma=0.67;
+	self.sigma=sigma;
+	if sigma<=20:
+		self.size=7;
+		self.tau=4.7;
+		self.num_iter=4;
+		self.model=MODEL("model7");
+	elif sigma<=40:
+		self.size=8;
+		self.tau=4.8;
+		self.num_iter=5;
+		self.model=MODEL("model8");
+	elif sigma<=60:
+		self.size=9;
+		self.tau=5.0;
+		self.num_iter=5;
+		self.model=MODEL("model9");
+	else:
+		self.size=10;
+		self.tau=5.2;
+		self.num_iter=6;
+		self.model=MODEL("model10");
+                      
+
+
+class MODEL(object):
+    # this class contain all the information about the model learned for the clustering
+    # this must contain the centre_vector the  Mahalanobi matrix for each class
+    # it also must contain the weight of the Gaussian Mixed Model
+    # receive also the number of classes K
+    def __init__(self,name):
+	mat_data=sio.loadmat('/auto/dtchome/celorag/Statistics_Project/para_gmm/'+name+'.mat')
+	self.Sigma=mat_data['model'][0][0][1];
+	self.weight=mat_data['model'][0][0][2];
+	self.size=self.weight.shape[-1];
+    def __getattr__(self,string):
+        for key in self.__dict__.keys():
+            if key.lower()==string.lower():
+                return self.__dict__[key]
+    def probability(self,Y,K,par,deviation=0):
+        sigma=self.Sigma[:,:,K-1]+deviation**2*np.identity(par.size**2);
+        prob = np.log(self.weight[1,K-1])-0.5*par.size^2*np.log(2*pi);
+        L=np.linalg.cholesky(sigma);
+        MHdistance=solve(L,Y);
+        prob -= sum(np.log(np.diagonal(L)))+0.5*sum(np.power(MHdistance,2));
+
+   
+
+
+def reassembling(Y,W):
+    n,m=par.original_size;
+    restore=np.zeros([n,m]);
+    weigth=np.zeros([n,m]);
+    for i in range(par.size):
+        for j in range(par.size):
+            k=par.size*i+j;
+            restore[i:-par.size+i+1,j:-par.size+j+1]=restore[i:-par.size+i+1,j:-par.size+j+1]+np.reshape(Y[:,k],[par.size,par.size]);
+            weigth[i:-par.size+i+1,j:-par.size+j+1]=weigth[i:-par.size+i+1,j:-par.size+j+1]++np.reshape(W[:,k],[par.size,par.size]);
+    restore=np.divide(restore,weigth+1e-16); 
+    
+
+
+def threshold_fun(a,b):
+    return np.sign(a)*np.maximum(np.abs(a)-b,0)
+
+def low_rank_approximation(Rx,K,par,sigma):
+        m=np.outer(np.means(Y,1), np.ones([1,Rx.shape[1]]));
+        Rx=Rx-m;
+        U,S,Vh=svd(Rx,full_matrice=False);
+        sv_Z=np.sqrt(np.maximum(np.power(S,2)/Rx.shape[1]-sigma**2,0));
+        threshold = par.tau*sigma^2*np.reciprocal(sv_Z+1e-16,dtype=float);
+        sv_Z = threshold_fun(sv_Z,threshold);
+        index = np.argwhere(sv_Z>0)[:,-1];
+        U=U[:,index];
+        Vh=Vh[index,:];
+        Z=U*np.diagonal(sv_z)*Vh;
+        if index[-1]==Rx.shape[0]:
+            
+            weight=1/Rx.shape[0];#weight used to reassemble the picture
+        else:
+            weigth=(Rx.shape[0]-index[-1])/Rx.shape[0];
+        W=weigth*np.ones(Z.shape);
+        Z=weigth*(Z+m);
+        return Z,W
+        
+        
+
+
+def patches(image,original,par):
+	(n,m)=par.original_size;
+	num_patches = (n-par.size+1)*(m-par.size+1);
+	Y=np.zeros([par.size**2,num_patches]); #current patches 
+	X=np.zeros(Y.shape); #patches extracted from the detection
+	k=0;
+
+	for i in range(par.size-1):
+		for j in range(par.size-1):
+	    
+		    k=par.size*i+j;
+		    
+		    Y[k,:]=np.reshape(image[i:-par.size+i+1,j:-par.size+j+1],-1);
+		    X[k,:]=np.reshape(original[i:-par.size+i+1,j:-par.size+j+1],-1);
+
+		k=par.size*i+j+1;
+		Y[k,:]=np.reshape(image[i:-par.size+i+1,j+1:],-1);
+		X[k,:]=np.reshape(original[i:-par.size+i+1,j+1:],-1);
+	for j in range(par.size-1):
+		k=par.size*(i+1)+j;
+		Y[k,:]=np.reshape(image[i+1:,j:-par.size+j+1],-1);
+		X[k,:]=np.reshape(original[i+1:,j:-par.size+j+1],-1);
+
+	Y[-1,:]=np.reshape(image[i+1:,j+1:],-1);
+	X[-1,:]=np.reshape(original[i+1:,j+1:],-1);
+
+	Sigma = np.sqrt(par.gamma*abs(par.sigma**2-np.mean(np.power(X-Y,2),0)));
+	return Y, Sigma
+    
+
+
+
+def cluster(Y,par,sigma_l):
+	model=par.model;
+	pdb.set_trace()
+	# for each patch we need to find the class that maximize the log-likelihood
+	# the learning has been done by subtracting the group mean
+	prob=np.zeros([model.size,Y.shape[-1]]);
+	Y=Y-np.outer(np.means(Y,0), np.ones([model.size**2,1]));
+	Y=Y/255.0; 
+	deviation=np.mean(sigma_l);
+	pdb.set_trace()
+	for i in range(model.size):
+		prob[i,:]=model.probability(Y,i,deviation);
+	
+	k=np.argmax(prob,axis=0); #class to which the patch belongs to 
+	group=[]
+	for i in range(model.size):
+		if np.sum(k==i)>10:
+		    group.append(i)
+	k=np.argmax(prob[group,:],axis=0); 
+	R={}
+	for item in group:
+		R[item]=np.argwhere(k==item);
+	return prob,R
+        
+
+
+
+def Denoising(noise_image,sigma,par):
+	z=noise_image;
+	for i in range(par.num_iter):
+		z=z+par.lmbda*(noise_image-z);
+		Y, Sigma_l=patches(z,noise_image,par);
+		if iter==1:
+			Sigma_l=par.sigma*ones(Sigma_l.shape);
+		f,h=cluster(Y,par,Sigma_l)
+
---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.stats import multivariate_normal
+from numpy.linalg import cholesky, solve, svd
+from functions import *
+import pdb
+from PIL import Image
+import scipy.io as sio
+
+picture = Image.open("peppers.png");
+real_picture = np.asarray(picture.convert('L'))
+#sigma_noise=input('Insert the variation of the noise:');
+sigma_noise=10;
+Y = real_picture+np.random.normal(0,sigma_noise,real_picture.shape);
+par=par_type(sigma_noise,original_size=Y.shape);
+
+Denoising(Y,sigma_noise,par)
